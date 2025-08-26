6acba5d6a41480914f827f9df651fd4239a23f8e
lemael <tchindefossomael@yahoo.fr>
2025-08-26 15:57:06 -0600

    url changed

--- ecommerce-frontend/src/utils/constants.ts
+++ ecommerce-frontend/src/utils/constants.ts
@@ -1,8 +1,19 @@
 // constants.ts
+/*
+// localhost
 const API_URL = "http://localhost:5045";
 const ORDERS_URL = `${API_URL}/orders`;
 const PRODUCTS_URL = `${API_URL}/products`;
 const BaseUrl = "http://localhost:5045/api";
+const InscriptionUrl = BaseUrl + "/users";
+const AnmeldenUrl = BaseUrl + "/auth/login";
+*/
+
+// render
+const API_URL = "https://ecommerce-project-2kvd.onrender.com";
+const ORDERS_URL = `${API_URL}/orders`;
+const PRODUCTS_URL = `${API_URL}/products`;
+const BaseUrl = "https://ecommerce-project-2kvd.onrender.com/api";
 const InscriptionUrl = BaseUrl + "/users";
 const AnmeldenUrl = BaseUrl + "/auth/login";
 

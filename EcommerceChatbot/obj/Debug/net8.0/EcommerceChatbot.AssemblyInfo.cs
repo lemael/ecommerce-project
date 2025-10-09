a93fdc2d89df345821fb14d146a4ff1a98616c2c
lemael <tchindefossomael@yahoo.fr>
2025-10-09 10:46:13 -0600

    update

--- EcommerceChatbot/obj/Debug/net8.0/EcommerceChatbot.AssemblyInfo.cs
+++ EcommerceChatbot/obj/Debug/net8.0/EcommerceChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc442674cabfba97699e9c403742c4e265def70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bee9d68630266b49579c54fc7bcdc29689a1f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
